92ee50e6205b4d7809830b4dc7dcfdfcd9e2b4c5
charuka deshan <itscharukadeshan@gmail.com>
2024-08-15 02:07:02 +0530

    feat✨: Add dns2_dialog_disable / enable feature

--- src/index.ts
+++ src/index.ts
@@ -22,6 +22,7 @@ const dialog_password = DIALOG_ROUTER_PASSWORD;
 
 const hutch_usr = HUTCH_ROUTER_USER_NAME;
 const hutch_password = HUTCH_ROUTER_PASSWORD;
+
 const app = express();
 
 const getBrowser = async (): Promise<Browser> =>
@@ -128,6 +129,130 @@ app.get("/restart_hutch", async (req, res) => {
 
     await page.waitForSelector("#yesbtn", { visible: true });
     await page.click("#yesbtn");
+
+    const screenshot = await page.screenshot();
+    res.end(screenshot, "binary");
+  } catch (error: any) {
+    if (!res.headersSent) {
+      res.status(400).send(error.message);
+    }
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
+});
+
+app.get("/dns2_dialog_enable", async (req, res) => {
+  let browser: Browser | null = null;
+
+  try {
+    browser = await getBrowser();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1280,
+      height: 800,
+    });
+    await page.goto(dialog_router, { waitUntil: "networkidle2" });
+
+    const loggedIn = await isLoggedIn(page);
+
+    if (!loggedIn) {
+      await page.waitForSelector("a#loginlink", { visible: true });
+      await page.click("a#loginlink");
+      await page.type("#txtUsr", dialog_usr);
+      await page.type("#txtPwd", dialog_password);
+      await page.click("#btnLogin");
+      await page.waitForSelector(
+        `a#logoutlink.margin-right-10[data-trans="logout"][data-bind*="logout"]`,
+        { visible: true }
+      );
+    }
+
+    await page.waitForSelector("a[onclick=\"tosms('#device_settings')\"]", {
+      visible: true,
+    });
+    await page.click("a[onclick=\"tosms('#device_settings')\"]");
+
+    await page.waitForSelector('a[data-trans="advance_settings"]', {
+      visible: true,
+    });
+
+    await page.click('a[data-trans="advance_settings"]');
+
+    await page.waitForSelector("div.row.form-group.is_show_dhcp_dns2");
+
+    await page.click(
+      'div.row.form-group.is_show_dhcp_dns2 input[name="dhcpServerDns"][value="1"]'
+    );
+
+    await page.click('input.btn.btn-primary[value="Apply"]');
+
+    await page.waitForSelector("input#yesbtn");
+
+    await page.click("input#yesbtn");
+
+    const screenshot = await page.screenshot();
+    res.end(screenshot, "binary");
+  } catch (error: any) {
+    if (!res.headersSent) {
+      res.status(400).send(error.message);
+    }
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
+});
+
+app.get("/dns2_dialog_disable", async (req, res) => {
+  let browser: Browser | null = null;
+
+  try {
+    browser = await getBrowser();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1280,
+      height: 800,
+    });
+    await page.goto(dialog_router, { waitUntil: "networkidle2" });
+
+    const loggedIn = await isLoggedIn(page);
+
+    if (!loggedIn) {
+      await page.waitForSelector("a#loginlink", { visible: true });
+      await page.click("a#loginlink");
+      await page.type("#txtUsr", dialog_usr);
+      await page.type("#txtPwd", dialog_password);
+      await page.click("#btnLogin");
+      await page.waitForSelector(
+        `a#logoutlink.margin-right-10[data-trans="logout"][data-bind*="logout"]`,
+        { visible: true }
+      );
+    }
+
+    await page.waitForSelector("a[onclick=\"tosms('#device_settings')\"]", {
+      visible: true,
+    });
+    await page.click("a[onclick=\"tosms('#device_settings')\"]");
+
+    await page.waitForSelector('a[data-trans="advance_settings"]', {
+      visible: true,
+    });
+
+    await page.click('a[data-trans="advance_settings"]');
+
+    await page.waitForSelector("div.row.form-group.is_show_dhcp_dns2");
+
+    await page.click(
+      'div.row.form-group.is_show_dhcp_dns2 input[name="dhcpServerDns"][value="0"]'
+    );
+
+    await page.click('input.btn.btn-primary[value="Apply"]');
+
+    await page.waitForSelector("input#yesbtn");
+
+    await page.click("input#yesbtn");
 
     const screenshot = await page.screenshot();
     res.end(screenshot, "binary");
